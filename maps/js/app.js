03bb3987f76e5da27694ab6414a124236823eb77
Min Kim <hyeonmin@uw.edu>
2016-11-17 12:10:56 -0800

    11/17/2016

--- maps/js/app.js
+++ maps/js/app.js
@@ -23,7 +23,7 @@ var osmTiles = {
 //for a list of map styles supported by Mapbox, as well
 //as full documentation about their map tiles API
 var mapboxTiles = {
-    accessToken: "...paste your access token here...",
+    accessToken: "pk.eyJ1IjoiaHllb25taW5rIiwiYSI6ImNpdm1uYmw1NjA5Nmoyb3BkMnZ6ejFjZHEifQ.1ARF7jv0qKBznrBgRfSJoA",
     url: "https://api.tiles.mapbox.com/v4/{style}/{z}/{x}/{y}.png?access_token={accessToken}",
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     styles: {
@@ -31,7 +31,9 @@ var mapboxTiles = {
         light: "mapbox.light",
         dark: "mapbox.dark",
         satellite: "mapbox.satellite",
-        pirates: "mapbox.pirates"
+        pirates: "mapbox.pirates",
+        comic: "mapbox.comic",
+        emerald: "mapbox.emerald"
     }
 };
 
@@ -48,4 +50,54 @@ var seattleCoords = [47.61, -122.33];
 //default zoom level (0-18 for street maps)
 //other map styles may have different zoom ranges
 var defaultZoom = 13;
+
+//start code
+var map = L.map(mapDiv).setView(seattleCoords, defaultZoom);
+L.tileLayer(mapboxTiles.url, {
+    attribution: mapboxTiles.attribution, 
+        style: mapboxTiles.styles.comic,
+        accessToken: mapboxTiles.accessToken
+}).addTo(map)
+
+function onPosition(position){
+    console.log(position);
+    var latlng = [position.coords.latitude, position.coords.longitude];
+    var marker= L.marker(latlng).addTo(map);
+
+}
+
+function onPositionError(err){
+    console.error(err);
+    alert(err.message);
+}
+
+if(navigator && navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onPosition, onPositionError);
+}
+
+fetch(seattle911API)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        data.forEach(function(record){
+            var latlng = [record.latitude, record.longitude]; 
+            var marker = L.circleMarker(latlng, {
+                // fillColor: "#F00",
+                // fillOpacity: .5,
+                color: record.type == "Aid Response" ? "BLACK" : record.type == "Auto Fire Alarm" ? "RED" : "BLUE",
+                radius: 5
+            }).addTo(map);
+            var html = "<h2>"+record.address+"</h2><p>"+ record.type +" "+ record.datetime+"</p>";
+            marker.bindPopup(html)
+        });
+    })
+    .catch(function(err) {
+        console.error(err);
+        alert(err.message);
+    });
+
+
+
 
